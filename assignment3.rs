abccd1665a4566da748d3d777b63fd1bdeb33ddc
stunlocke <mnorris@calpoly.edu>
2015-05-31 16:51:40 -0700

    Changed to use enum for EXCR3

--- assignment3.rs
+++ assignment3.rs
@@ -1,5 +1,5 @@
 
-struct binOpC {
+/*struct binOpC {
    op: String,
    left: Option<Box<EXCR3>>,
    right: Option<Box<EXCR3>>
@@ -19,17 +19,19 @@ struct appC {
 struct lamC {
    body: Option<Box<EXCR3>>,
    args: Vec<String>
+}*/
+
+enum EXCR3 {
+    i32,
+    binOpC { op: String, left: Option<Box<EXCR3>>, right: Option<Box<EXCR3>> },
+    bool,
+    ifC { test: Option<Box<EXCR3>>, then: Option<Box<EXCR3>>, els: Option<Box<EXCR3>> },
+    String,
+    appC { fun: Option<Box<EXCR3>>, args: Vec<Option<Box<EXCR3>>> },
+    lamC { body: Option<Box<EXCR3>>, args: Vec<String> }
 }
 
-struct EXCR3 {
-    numC: i32,
-    binOpC: Option<Box<binOpC>>,
-    boolC: bool,
-    ifC: Option<Box<ifC>>,
-    idC: String,
-    appC: Option<Box<appC>>,
-    lamC: Option<Box<lamC>>
-}
+// Michael: nums, with
 
 fn main() {
    println!("Hello, world!");
